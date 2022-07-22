e6158319e9fdb2449a14cb1298d4d56955541ec3
aoyama_saito <saitomarimo1229@gmail.com>
2022-07-22 18:39:56 +0900

    [add] Stage作成用のEditor拡張勉強中

---
+++ Assets/Editor/StageEditor.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class StageEditor : EditorWindow
+{
+    [MenuItem("Editor/StageEditor")]
+    private static void WindowCreate()
+    {
+        // ê∂ê¨
+        StageEditor window = GetWindow<StageEditor>("StageEditor");
+        // ç≈è¨ÉTÉCÉYê›íË
+        window.minSize = new Vector2(320, 320);
+    }
+
+    private void Create()
+    {
+
+    }
+
+    private void Save()
+    {
+
+    }
+}
