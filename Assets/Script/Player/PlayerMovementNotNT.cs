799a30b43af54b3a9c9a4933b0a8457463cb04f4
TakaiKentarou <mmpm664@yahoo.co.jp>
2022-08-01 19:03:42 +0900

    [update]改良

--- Assets/Script/Player/PlayerMovementNotNT.cs
+++ Assets/Script/Player/PlayerMovementNotNT.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// プレイヤーの動きを制御するコンポーネント
@@ -8,18 +9,21 @@ public class PlayerMovementNotNT : MonoBehaviour
 {
     [SerializeField] float _speed = 5f;
     [SerializeField] float _jumpSpeed = 5f;
-    Rigidbody2D _rb;
-    SpriteRenderer _sprite;
-    bool _isGrounded = false;
+    [SerializeField] float _entryTime;
 
+    [SerializeField,Tooltip("Quick match")] GameObject _quickMatch;
+    [SerializeField,Tooltip("TakaiTestScene")] TakaiTestScene _scene;
+    Rigidbody2D _rb;
+    [SerializeField] Image _image;
+    bool _isGrounded = false;
+    bool _isEnterded = false;
+    float ratio;
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        _sprite = GetComponent<SpriteRenderer>();
-
-        //GameManager.Instance?.SetPlayer(gameObject);
     }
 
+    
     private void Update()
     {
         float h = Input.GetAxisRaw("Horizontal");
@@ -31,8 +35,37 @@ public class PlayerMovementNotNT : MonoBehaviour
             velocity.y = _jumpSpeed;
             _isGrounded = false;
         }
-
         _rb.velocity = velocity;
+
+        if (_isEnterded)
+        {
+            ratio += Time.deltaTime / _entryTime;
+            _image.fillAmount = ratio;
+
+            if(_image.fillAmount >= 1)
+            {
+                _scene.OnClickEnterGame();
+            }
+        }
+        else
+        {
+            ratio = 0;
+            _image.fillAmount = 0;
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(_quickMatch)
+        {
+            _isEnterded = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _isEnterded = false;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
